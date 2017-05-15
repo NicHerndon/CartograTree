0900eac56922d94af5b171d3e330753e6c87c810
NicHerndon <nic.herndon@uconn.edu>
2017-05-15 15:41:40 -0400

    Updated cartogratree_layers_show_select_changed in cartogratree.js.

--- cartogratree.js
+++ cartogratree.js
@@ -1,97 +1,77 @@
+var cartogratree_mid_layer = [new ol.layer.Tile({opacity: 0.8}), new ol.layer.Tile({opacity: 0.8}), new ol.layer.Tile({opacity: 0.8}), new ol.layer.Tile({opacity: 0.8})];
+var cartogratree_gis;
+
 (function ($) {
     Drupal.behaviors.cartogratree = {
         attach: function (context, settings) {
-            var gis = Drupal.settings.cartogratree.gis;
-            var common_view = new ol.View({
+            cartogratree_gis = Drupal.settings.cartogratree.gis;
+            var cartogratree_common_view = new ol.View({
                 center: ol.proj.fromLonLat([10, 20]),
                 zoom: 2
             });
-            var osm_layer = new ol.layer.Tile({
+            var cartogratree_osm_layer = new ol.layer.Tile({
                 source: new ol.source.OSM()
             });
-            var trees_layer = new ol.layer.Tile({
+            var cartogratree_trees_layer = new ol.layer.Tile({
                 source: new ol.source.TileWMS({
-                    url: gis,
+                    url: cartogratree_gis,
                     params: {LAYERS: 'ct:sample'}
                 })
             })
+
             new ol.Map({
-                view: common_view,
+                view: cartogratree_common_view,
                 layers: [
                     // 'background' map
-                    osm_layer,
-                    // min temperature - raster
-                    tmin_layer = new ol.layer.Tile({
-                        source: new ol.source.TileWMS({
-                            url: gis,
-                            attributions: "Minimum temperature (°C) for January averaged over 1970-2000, at 10' spatial resolution, &copy; <a href=\"http://worldclim.org/version2\">WorldClim_v2</a>.",
-                            params: {LAYERS: 'ct:wc2.0_10m_tmin_01'},
-                        }),
-                        opacity: 0.5,
-                    }),
+                    cartogratree_osm_layer,
+                    // middle layer
+                    cartogratree_mid_layer[0],
                     // trees - vector
-                    trees_layer,
+                    cartogratree_trees_layer,
                 ],
+                controls: [new ol.control.ScaleLine(), new ol.control.Attribution()],
                 target: 'cartogratree_top_left'
             });
 
             new ol.Map({
-                view: common_view,
+                view: cartogratree_common_view,
                 layers: [
                     // 'background' map
-                    osm_layer,
-                    // precipitation - raster
-                    treesLyr = new ol.layer.Tile({
-                        source: new ol.source.TileWMS({
-                            url: gis,
-                            attributions: "Water vapor pressure (kPa) for January averaged over 1970-2000, at 10' spatial resolution, &copy; <a href=\"http://worldclim.org/version2\">WorldClim_v2</a>.",
-                            params: {LAYERS: 'ct:wc2.0_10m_prec_01'},
-                        }),
-                        opacity: 0.5,
-                    }),
+                    cartogratree_osm_layer,
+                    // middle layer
+                    cartogratree_mid_layer[1],
                     // trees - vector
-                    trees_layer,
+                    cartogratree_trees_layer,
                 ],
+                controls: [new ol.control.ScaleLine(), new ol.control.Attribution()],
                 target: 'cartogratree_top_right'
             });
 
             new ol.Map({
-                view: common_view,
+                view: cartogratree_common_view,
                 layers: [
                     // 'background' map
-                    osm_layer,
-                    // wind speed - raster
-                    treesLyr = new ol.layer.Tile({
-                        source: new ol.source.TileWMS({
-                            url: gis,
-                            attributions: "Wind speed (m s-1) for January averaged over 1970-2000, at 10' spatial resolution, &copy; <a href=\"http://worldclim.org/version2\">WorldClim_v2</a>.",
-                            params: {LAYERS: 'ct:wc2.0_10m_wind_01'},
-                        }),
-                        opacity: 0.5,
-                    }),
+                    cartogratree_osm_layer,
+                    // middle layer
+                    cartogratree_mid_layer[2],
                     // trees - vector
-                    trees_layer,
+                    cartogratree_trees_layer,
                 ],
+                controls: [new ol.control.ScaleLine(), new ol.control.Attribution()],
                 target: 'cartogratree_bottom_left'
             });
 
             new ol.Map({
-                view: common_view,
+                view: cartogratree_common_view,
                 layers: [
                     // 'background' map
-                    osm_layer,
-                    // soils - polygon
-                    treesLyr = new ol.layer.Tile({
-                        source: new ol.source.TileWMS({
-                            url: gis,
-                            attributions: "Harmonized World Soil Dataset - Major Soil Groups (Data Basin Dataset), &copy; <a href=\"https://databasin.org\">Data Basin</a>.",
-                            params: {LAYERS: 'ct:global_soils_merge_psql'},
-                        }),
-                        opacity: 0.5,
-                    }),
+                    cartogratree_osm_layer,
+                    // middle layer
+                    cartogratree_mid_layer[3],
                     // trees - vector
-                    trees_layer,
+                    cartogratree_trees_layer,
                 ],
+                controls: [new ol.control.ScaleLine(), new ol.control.Attribution()],
                 target: 'cartogratree_bottom_right'
             });
         }
@@ -131,5 +111,82 @@ function cartogratree_layers () {
     else {
         list.style.display = 'none';
         document.getElementById("cartogratree_layers_arrow").innerHTML = "&#9660;";
+    }
+}
+
+/* Update the layers shown based on user's selection */
+function cartogratree_layers_show_select_changed () {
+    var selected = document.getElementById("cartogratree_layers_show_select");
+    var shown_layers = document.getElementsByName('cartogratree_shown_layers')[0].value;
+    var prev_shown_layers = (shown_layers == "") ? [] : shown_layers.split(',');
+    
+    if (prev_shown_layers.length < selected.selectedOptions.length) {
+        // there are more entries than previously
+        if (selected.selectedOptions.length < 5) {
+            // add layer to shown layers
+            for (var i = 0; i < selected.selectedOptions.length; i++) {
+                if (prev_shown_layers.indexOf(selected.selectedOptions[i].index.toString()) == -1) {
+                    prev_shown_layers.push(selected.selectedOptions[i].index);
+                    // add layer to map
+                    var value = selected.selectedOptions[i].value;
+                    var attr = " &copy; <a href=\"".concat(selected.selectedOptions[i].text, "\">", selected.selectedOptions[i].label, "</a>.");
+                    cartogratree_mid_layer[prev_shown_layers.length - 1].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
+                }
+            }
+            document.getElementsByName('cartogratree_shown_layers')[0].value = prev_shown_layers.join();
+        }
+        else {
+            // max is 4, unselect the last one
+            alert("Only four layers can be shown! Unselect another shown layer and try again.");
+            for (var i = 0; i < selected.selectedOptions.length; i++) {
+                if (prev_shown_layers.indexOf(selected.selectedOptions[i].index.toString()) == -1) {
+                    selected.selectedOptions[i].selected = false;
+                }
+            }
+        }
+    }
+    else if (prev_shown_layers.length > selected.selectedOptions.length) {
+        // there are less entries than previously
+        // get the indexes of the selected layers
+        var sel_indexes = [];
+        for (var i = 0; i < selected.selectedOptions.length; i++) {
+            sel_indexes.push(selected.selectedOptions[i].index.toString());
+        }
+        // make a copy of prev_shown_layers
+        var cp_prev_shown_layers = prev_shown_layers.slice();
+        // remove previously selected indexes, not currently selected
+        for (i in cp_prev_shown_layers) {
+            if (sel_indexes.indexOf(cp_prev_shown_layers[i]) == -1) {
+                prev_shown_layers.splice(prev_shown_layers.indexOf(cp_prev_shown_layers[i]), 1);
+            }
+        }
+        // if needed, add currently selected indexes to previously selected indexes
+        for (var i = 0; i < selected.selectedOptions.length; i++) {
+                if (prev_shown_layers.indexOf(selected.selectedOptions[i].index.toString()) == -1) {
+                    prev_shown_layers.push(selected.selectedOptions[i].index);
+                }
+        }
+        document.getElementsByName('cartogratree_shown_layers')[0].value = prev_shown_layers.join();
+        // update the maps
+        for (var i = 0; i < 4; i++) {
+            if (prev_shown_layers[i] === undefined) {
+                cartogratree_mid_layer[i].setSource(null); 
+            }
+            else {
+                if (cartogratree_mid_layer[i].getProperties().source.ec != "LAYERS-".concat(selected[prev_shown_layers[i]].value)) {
+                    var value = selected[prev_shown_layers[i]].value;
+                    var attr = " &copy; <a href=\"".concat(selected.selectedOptions[i].text, "\">", selected.selectedOptions[i].label, "</a>.");
+                    cartogratree_mid_layer[prev_shown_layers.length - 1].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
+                }
+            }
+        }
+    }
+    else {
+        // there are the same number of entries as previously (i.e., one)
+        document.getElementsByName('cartogratree_shown_layers')[0].value = selected.selectedOptions[0].index;
+        // update the layer of the first map
+        var value = selected.selectedOptions[0].value;
+        var attr = " &copy; <a href=\"".concat(selected.selectedOptions[i].text, "\">", selected.selectedOptions[i].label, "</a>.");
+        cartogratree_mid_layer[0].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
     }
 }
