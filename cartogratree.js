9d6f25eb030588cdeae92f09a63e67a57d05edd7
NicHerndon <nic.herndon@uconn.edu>
2017-05-30 16:49:49 -0400

    Converted most JS code to jQuery.

--- cartogratree.js
+++ cartogratree.js
@@ -3,18 +3,18 @@
  * Implements the dynamic functionality of the CartograTree app (i.e., ?q=cartogratree/app).
  */
 
-var cartogratree_mid_layer = [new ol.layer.Tile({opacity: 0.8}), new ol.layer.Tile({opacity: 0.8}), new ol.layer.Tile({opacity: 0.8}), new ol.layer.Tile({opacity: 0.8})];
-var cartogratree_map = new Array(4);
-var target = ['cartogratree_top_left', 'cartogratree_top_right', 'cartogratree_bottom_left', 'cartogratree_bottom_right'];
-var cartogratree_gis;
-
 /**
  * Attach the maps to the four squares on the app page.
  */
 (function ($) {
     Drupal.behaviors.cartogratree = {
         attach: function (context, settings) {
-            cartogratree_gis = Drupal.settings.cartogratree.gis;
+            'use strict';
+            
+            var cartogratree_mid_layer = [new ol.layer.Tile({opacity: 0.8}), new ol.layer.Tile({opacity: 0.8}), new ol.layer.Tile({opacity: 0.8}), new ol.layer.Tile({opacity: 0.8})];
+            var cartogratree_map = new Array(4);
+            var target = ['cartogratree_top_left', 'cartogratree_top_right', 'cartogratree_bottom_left', 'cartogratree_bottom_right'];
+            var cartogratree_gis = Drupal.settings.cartogratree.gis;
             var cartogratree_common_view = new ol.View({
                 center: ol.proj.fromLonLat([10, 20]),
                 zoom: 2
@@ -62,19 +62,17 @@ var cartogratree_gis;
                     // close navigation
                     $("#cartogratree_sidenav").width("0px");
                     // enable page scrolling
-                    document.body.style.overflow = "";
+                    $('html, body').css({overflow: 'auto'});
                 } else {
                     // set navigation position
-                    var $top = $('#cartogratree_top_left')[0].getBoundingClientRect().top;
-                    var $left = $('#cartogratree_top_left')[0].getBoundingClientRect().left;
-                    var $height = $('#cartogratree_top_left')[0].getBoundingClientRect().bottom - $top;
-                    $("#cartogratree_sidenav").css({ top: $top + 'px' });
-                    $("#cartogratree_sidenav").css({ left: $left + 'px' });
-                    $("#cartogratree_sidenav").height($height + 'px');
+                    var top = $('#cartogratree_top_left')[0].getBoundingClientRect().top;
+                    var left = $('#cartogratree_top_left')[0].getBoundingClientRect().left;
+                    var height = $('#cartogratree_top_left')[0].getBoundingClientRect().bottom - top;
+                    $("#cartogratree_sidenav").css({ top: top + 'px', left: left + 'px', height: height + 'px'});
                     // open navigation
                     $("#cartogratree_sidenav").width("250px");
                     // disable page scrolling
-                    document.body.style.overflow = "hidden";
+                    $('html, body').css({overflow: 'hidden'});
                 }
             });
             
@@ -90,155 +88,90 @@ var cartogratree_gis;
                 }
             });
             
-//            $("#cartogratree_layers_show_select").change(function() {
-//                var $selected = $("#cartogratree_layers_show_select");
-//                var $shown_layers = $('[name="cartogratree_shown_layers"]')[0].value;
-//                var $prev_shown_layers = ($shown_layers == "") ? [] : $shown_layers.split(',');
-//
-//                if ($prev_shown_layers.length < $selected.selectedOptions.length) {
-//                    // there are more entries than previously
-//                    if ($selected.selectedOptions.length < 5) {
-//                        // add layer to shown layers
-//                        for (var i = 0; i < $selected.selectedOptions.length; i++) {
-//                            if ($prev_shown_layers.indexOf($selected.selectedOptions[i].index.toString()) == -1) {
-//                                $prev_shown_layers.push($selected.selectedOptions[i].index);
-//                                // add layer to map
-//                                var $value = $selected.selectedOptions[i].value;
-//                                var attr = " &copy; <a href=\"".concat($selected.selectedOptions[i].text, "\">", $selected.selectedOptions[i].label, "</a>");
-//                                cartogratree_mid_layer[$prev_shown_layers.length - 1].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
-//                            }
-//                        }
-//                        $('[name="cartogratree_shown_layers"]')[0].value = $prev_shown_layers.join();
-//                    } else {
-//                        // max is 4, unselect the last one
-//                        alert("Only four layers can be shown! Unselect another shown layer and try again.");
-//                        for (var i = 0; i < $selected.selectedOptions.length; i++) {
-//                            if ($prev_shown_layers.indexOf($selected.selectedOptions[i].index.toString()) == -1) {
-//                                $selected.selectedOptions[i].selected = false;
-//                            }
-//                        }
-//                    }
-//                } else if ($prev_shown_layers.length > $selected.selectedOptions.length) {
-//                    // there are less entries than previously
-//                    // get the indexes of the selected layers
-//                    var sel_indexes = [];
-//                    for (var i = 0; i < $selected.selectedOptions.length; i++) {
-//                        sel_indexes.push($selected.selectedOptions[i].index.toString());
-//                    }
-//                    // make a copy of prev_shown_layers
-//                    var cp_prev_shown_layers = $prev_shown_layers.slice();
-//                    // remove previously selected indexes, not currently selected
-//                    for (i in cp_prev_shown_layers) {
-//                        if (sel_indexes.indexOf(cp_prev_shown_layers[i]) == -1) {
-//                            $prev_shown_layers.splice($prev_shown_layers.indexOf(cp_prev_shown_layers[i]), 1);
-//                        }
-//                    }
-//                    // if needed, add currently selected indexes to previously selected indexes
-//                    for (var i = 0; i < $selected.selectedOptions.length; i++) {
-//                        if ($prev_shown_layers.indexOf($selected.selectedOptions[i].index.toString()) == -1) {
-//                            $prev_shown_layers.push($selected.selectedOptions[i].index);
-//                        }
-//                    }
-//                    $('[name="cartogratree_shown_layers"]')[0].value = $prev_shown_layers.join();
-//                    // update the maps
-//                    for (var i = 0; i < 4; i++) {
-//                        if ($prev_shown_layers[i] === undefined) {
-//                            cartogratree_mid_layer[i].setSource(null);
-//                        } else {
-//                            if (cartogratree_mid_layer[i].getProperties().source.ec != "LAYERS-".concat($selected[$prev_shown_layers[i]].value)) {
-//                                var value = $selected[$prev_shown_layers[i]].value;
-//                                var attr = " &copy; <a href=\"".concat($selected.selectedOptions[i].text, "\">", $selected.selectedOptions[i].label, "</a>.");
-//                                cartogratree_mid_layer[$prev_shown_layers.length - 1].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
-//                            }
-//                        }
-//                    }
-//                } else {
-//                    // there are the same number of entries as previously (i.e., one)
-//                    $('[name="cartogratree_shown_layers"]')[0].value = $selected.selectedOptions[0].index;
-//                    // update the layer of the first map
-//                    var value = $selected.selectedOptions[0].value;
-//                    var attr = " &copy; <a href=\"".concat($selected.selectedOptions[0].text, "\">", $selected.selectedOptions[0].label, "</a>.");
-//                    cartogratree_mid_layer[0].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
-//                }
-//            });
+            /**
+             * Update the layers shown based on user's selection. The layers are mapped to the
+             * squares/maps in the order they were selected. The first layer selected is mapped
+             * on the top-left square, the second one to the top-right square, the third one on
+             * the bottom-left square, and the last one on the bottom-right square.
+             */
+            $("#cartogratree_layers_show_select").change(function() {
+                var $select = $("#cartogratree_layers_show_select");
+                var selected = ($select.val()) ? $select.val().length : 0;
+                var shown_layers = $('#cartogratree_shown_layers').attr('value');
+                var prev_shown_layers = (shown_layers == "") ? [] : shown_layers.split(',');
+
+                if (prev_shown_layers.length < selected) {
+                    // there are more entries than previously
+                    if (selected < 5) {
+                        // add layer to shown layers
+                        for (var i = 0; i < selected; i++) {
+                            if (prev_shown_layers.indexOf($select[0].selectedOptions[i].index.toString()) == -1) {
+                                prev_shown_layers.push($select[0].selectedOptions[i].index.toString());
+                                // add layer to map
+                                var value = $select.val()[i];
+                                var attr = " &copy; <a href=\"".concat($select.find('option:selected')[i].text, "\">", $select.find('option:selected')[i].label, "</a>");
+                                cartogratree_mid_layer[prev_shown_layers.length - 1].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
+                            }
+                        }
+                        $('#cartogratree_shown_layers').attr('value', prev_shown_layers.join());
+                    }
+                    else {
+                        // max is 4, unselect the last one
+                        alert("Only four layers can be shown! Unselect another shown layer and try again.");
+                        var fifth;
+                        for (var i = 0; i < selected; i++) {
+                            if (prev_shown_layers.indexOf($select[0].selectedOptions[i].index.toString()) == -1) {
+                                fifth = $select[0].selectedOptions[i].index;
+                            }
+                        }
+                        $select.find('option:selected')[fifth].selected = false;
+                    }
+                }
+                else if (prev_shown_layers.length > selected) {
+                    // there are less entries than previously
+                    // get the indexes of the selected layers
+                    var sel_indexes = [];
+                    for (var i = 0; i < selected; i++) {
+                        sel_indexes.push($select[0].selectedOptions[i].index.toString());
+                    }
+                    // make a copy of prev_shown_layers
+                    var cp_prev_shown_layers = prev_shown_layers.slice();
+                    // remove previously selected indexes, not currently selected
+                    for (i in cp_prev_shown_layers) {
+                        if (sel_indexes.indexOf(cp_prev_shown_layers[i]) == -1) {
+                            prev_shown_layers.splice(prev_shown_layers.indexOf(cp_prev_shown_layers[i]), 1);
+                        }
+                    }
+                    // if needed, add currently selected indexes to previously selected indexes
+                    for (var i = 0; i < selected; i++) {
+                        if (prev_shown_layers.indexOf($select[0].selectedOptions[i].index.toString()) == -1) {
+                            prev_shown_layers.push($select[0].selectedOptions[i].index.toString());
+                        }
+                    }
+                    $('#cartogratree_shown_layers').attr('value', prev_shown_layers.join());
+                    // update the maps
+                    for (var i = 0; i < 4; i++) {
+                        if (prev_shown_layers[i] === undefined) {
+                            cartogratree_mid_layer[i].setSource(null);
+                        }
+                        else {
+                            if (cartogratree_mid_layer[i].getProperties().source.ec != "LAYERS-".concat($select[0][parseInt(prev_shown_layers[i])].value)) {
+                                var value = $select.val()[i];
+                                var attr = " &copy; <a href=\"".concat($select.find('option:selected')[i].text, "\">", $select.find('option:selected')[i].label, "</a>");
+                                cartogratree_mid_layer[prev_shown_layers.length - 1].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
+                            }
+                        }
+                    }
+                }
+                else {
+                    // there are the same number of entries as previously (i.e., one)
+                    $('#cartogratree_shown_layers').attr('value', $select[0].selectedOptions[0].index.toString());
+                    // update the layer of the first map
+                    var value = $select.val()[0];
+                    var attr = " &copy; <a href=\"".concat($select.find('option:selected')[0].text, "\">", $select.find('option:selected')[0].label, "</a>");
+                    cartogratree_mid_layer[0].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
+                }
+            });
         },
     };
 }(jQuery));
-
-/**
- * Update the layers shown based on user's selection. The layers are mapped to the
- * squares/maps in the order they were selected. The first layer selected is mapped
- * on the top-left square, the second one to the top-right square, the third one on
- * the bottom-left square, and the last one on the bottom-right square.
- */
-function cartogratree_layers_show_select_changed() {
-    var selected = document.getElementById("cartogratree_layers_show_select");
-    var shown_layers = document.getElementsByName('cartogratree_shown_layers')[0].value;
-    var prev_shown_layers = (shown_layers == "") ? [] : shown_layers.split(',');
-
-    if (prev_shown_layers.length < selected.selectedOptions.length) {
-        // there are more entries than previously
-        if (selected.selectedOptions.length < 5) {
-            // add layer to shown layers
-            for (var i = 0; i < selected.selectedOptions.length; i++) {
-                if (prev_shown_layers.indexOf(selected.selectedOptions[i].index.toString()) == -1) {
-                    prev_shown_layers.push(selected.selectedOptions[i].index);
-                    // add layer to map
-                    var value = selected.selectedOptions[i].value;
-                    var attr = " &copy; <a href=\"".concat(selected.selectedOptions[i].text, "\">", selected.selectedOptions[i].label, "</a>");
-                    cartogratree_mid_layer[prev_shown_layers.length - 1].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
-                }
-            }
-            document.getElementsByName('cartogratree_shown_layers')[0].value = prev_shown_layers.join();
-        } else {
-            // max is 4, unselect the last one
-            alert("Only four layers can be shown! Unselect another shown layer and try again.");
-            for (var i = 0; i < selected.selectedOptions.length; i++) {
-                if (prev_shown_layers.indexOf(selected.selectedOptions[i].index.toString()) == -1) {
-                    selected.selectedOptions[i].selected = false;
-                }
-            }
-        }
-    } else if (prev_shown_layers.length > selected.selectedOptions.length) {
-        // there are less entries than previously
-        // get the indexes of the selected layers
-        var sel_indexes = [];
-        for (var i = 0; i < selected.selectedOptions.length; i++) {
-            sel_indexes.push(selected.selectedOptions[i].index.toString());
-        }
-        // make a copy of prev_shown_layers
-        var cp_prev_shown_layers = prev_shown_layers.slice();
-        // remove previously selected indexes, not currently selected
-        for (i in cp_prev_shown_layers) {
-            if (sel_indexes.indexOf(cp_prev_shown_layers[i]) == -1) {
-                prev_shown_layers.splice(prev_shown_layers.indexOf(cp_prev_shown_layers[i]), 1);
-            }
-        }
-        // if needed, add currently selected indexes to previously selected indexes
-        for (var i = 0; i < selected.selectedOptions.length; i++) {
-            if (prev_shown_layers.indexOf(selected.selectedOptions[i].index.toString()) == -1) {
-                prev_shown_layers.push(selected.selectedOptions[i].index);
-            }
-        }
-        document.getElementsByName('cartogratree_shown_layers')[0].value = prev_shown_layers.join();
-        // update the maps
-        for (var i = 0; i < 4; i++) {
-            if (prev_shown_layers[i] === undefined) {
-                cartogratree_mid_layer[i].setSource(null);
-            } else {
-                if (cartogratree_mid_layer[i].getProperties().source.ec != "LAYERS-".concat(selected[prev_shown_layers[i]].value)) {
-                    var value = selected[prev_shown_layers[i]].value;
-                    var attr = " &copy; <a href=\"".concat(selected.selectedOptions[i].text, "\">", selected.selectedOptions[i].label, "</a>.");
-                    cartogratree_mid_layer[prev_shown_layers.length - 1].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
-                }
-            }
-        }
-    } else {
-        // there are the same number of entries as previously (i.e., one)
-        document.getElementsByName('cartogratree_shown_layers')[0].value = selected.selectedOptions[0].index;
-        // update the layer of the first map
-        var value = selected.selectedOptions[0].value;
-        var attr = " &copy; <a href=\"".concat(selected.selectedOptions[0].text, "\">", selected.selectedOptions[0].label, "</a>.");
-        cartogratree_mid_layer[0].setSource(new ol.source.TileWMS({url: cartogratree_gis, attributions: attr, params: {LAYERS: value}}));
-    }
-}
