127e81f7ac1453aa758bc22fe42f2f4e9dd9a18c
NicHerndon <nic.herndon@uconn.edu>
2017-08-30 14:57:47 -0400

    Changed the path to the app, and tested various options to preprocess for template.

---
+++ theme/templates/page--cartogratree.tpl.php
@@ -0,0 +1,9 @@
+<?php dpm($variables); print drupal_render($page['content']); ?>
+
+<?php
+//  print '<pre>'; print "messages"
+//  dpm(get_defined_vars());
+//  dpm($theme_hook_suggestions);
+//  dpm(array_keys(theme_get_registry()));
+//  print '</pre>';
+?>
