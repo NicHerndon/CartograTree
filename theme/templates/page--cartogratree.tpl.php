842919110fee1d72e1fe4bed1e9a61d515c10f39
NicHerndon <nic.herndon@uconn.edu>
2017-09-19 15:02:50 -0400

    Created functions for page template.

--- theme/templates/page--cartogratree.tpl.php
+++ theme/templates/page--cartogratree.tpl.php
@@ -1,11 +1,12 @@
 <?php
+//    dpm(get_defined_vars());
 //    dpm($variables);
     print drupal_render($page['content']);
  ?>
 
 <?php
 //  print '<pre>'; print "messages"
-//  dpm(get_defined_vars());
+  dpm(get_defined_vars());
 //  dpm($theme_hook_suggestions);
 //  dpm(array_keys(theme_get_registry()));
 //  print '</pre>';
