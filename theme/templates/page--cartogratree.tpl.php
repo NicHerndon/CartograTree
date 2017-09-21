7b63d94610232eb12fc88efb95c43e839fd8b50e
NicHerndon <nic.herndon@uconn.edu>
2017-09-21 14:25:48 -0400

    Removed some commented code.

--- theme/templates/page--cartogratree.tpl.php
+++ theme/templates/page--cartogratree.tpl.php
@@ -1,12 +1,11 @@
 <?php
-//    dpm(get_defined_vars());
-//    dpm($variables);
     print drupal_render($page['content']);
  ?>
 
 <?php
 //  print '<pre>'; print "messages"
-  dpm(get_defined_vars());
+//  dpm(get_defined_vars());
+//  dpm($variables);
 //  dpm($theme_hook_suggestions);
 //  dpm(array_keys(theme_get_registry()));
 //  print '</pre>';
